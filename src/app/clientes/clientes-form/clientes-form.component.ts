3b8c48c8e7f9a14dca165c34613c951af9918b23
Riosney <rsantosdo@minsait.com>
2022-10-10 14:21:14 -0300

    Testando algumas formas de binding

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -10,10 +10,18 @@ import { Cliente } from '../clientes';
 export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
+  nome: string = 'Fulano';
 
-  constructor() { }
+  constructor() { 
+    this.cliente = new Cliente();
+    this.cliente.nome = 'Rios';
+  }
 
   ngOnInit(): void {
+  }
+
+  clicar(){
+    console.log('cliquei');
   }
 
 }
--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
 
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component';
 
 
 @NgModule({
-  declarations: [ClientesFormComponent],
+  declarations: [
+    ClientesFormComponent
+  ],
   imports: [
     CommonModule,
-    ClientesRoutingModule
+    ClientesRoutingModule,
+    FormsModule
   ], exports: [
     ClientesFormComponent
   ]
