be405d60608307a0c990532c373ae1da0cc729e4
Riosney <rsantosdo@minsait.com>
2022-10-11 14:50:08 -0300

    Criando e injetando o serviço de clientes

---
+++ src/app/clientes.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Cliente } from './clientes/clientes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor() { }
+
+  getCliente() : Cliente {
+    let cliente : Cliente = new Cliente();
+    cliente.nome = 'Fulado de tal';
+    cliente.cpf = '11122233344'
+    return cliente;
+  }
+}
