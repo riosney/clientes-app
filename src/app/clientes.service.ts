390854ea9d5878dd1a34358deb8f822a2219a8f1
Riosney <rsantosdo@minsait.com>
2022-10-11 16:07:10 -0300

    Criação do método salvar clientes

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
 import { Cliente } from './clientes/cliente';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,12 @@ import { Cliente } from './clientes/cliente';
 export class ClientesService {
 
   constructor( private http: HttpClient ) {
-    
-   }
+
+  }
+
+  salvar( cliente: Cliente) : Observable<Cliente>{
+    return this.http.post<Cliente>('http://localhost:8081/api/clientes', cliente);
+  }
 
   getCliente() : Cliente {
     let cliente : Cliente = new Cliente();
